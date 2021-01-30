10e6821a1aec8e4ba8eb14395391247e5c427a09
arrows65 <78029315+arrows65@users.noreply.github.com>
2021-01-30 22:53:55 +0900

    follow with pep8

--- get-quote.py
+++ get-quote.py
@@ -1,28 +1,28 @@
 import random
+
+
 def primary():
-  # print("Keep it logically awesome.")
+    # print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
 
-  chosenList = []
+    chosenList = []
 
-  for i in range(random.randint(1,len(quotes))):
-    rnd = random.randint(0, len(quotes)-1)
-    chosen = quotes[rnd]
-  
-    if chosen[-1:] == '\n':
-      chosen = chosen[:-1]
-    
-    if chosenList.count(chosen) == 0:
-      chosenList.append(chosen)
+    for i in range(random.randint(1, len(quotes))):
+        rnd = random.randint(0, len(quotes)-1)
+        chosen = quotes[rnd]
 
-  for i in chosenList:
-    print(i)
+        if chosen[-1:] == '\n':
+            chosen = chosen[:-1]
+
+        if chosenList.count(chosen) == 0:
+            chosenList.append(chosen)
+
+    for i in chosenList:
+        print(i)
 
 
-  
-
-if __name__== "__main__":
-  primary()
+if __name__ == "__main__":
+    primary()
